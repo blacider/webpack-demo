b900f74013df741742fc572cacd87708f93198d2
blacider <niurenpeng@niurenpeng.com>
2018-06-26 20:54:38 +0800

    [update] 生产和开发环境配置分离

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import _ from 'lodash';
+import printMe from './print.js';
+import './style.css';
 
 function component() {
     var element = document.createElement('div');
-  
+    var btn = document.createElement('button');
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
+    btn.innerHTML = '点我';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element;
   }
   
-  document.body.appendChild(component());
+  let element = component();
+  document.body.appendChild(element);
+
+  if (module.hot) {
+    module.hot.accept('./print.js', function () {
+      console.log('收到print更新！');
+      element = component();
+      document.body.appendChild(element);
+
+    })
+  }
