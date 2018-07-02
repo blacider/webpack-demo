c2345c994b0ee776f7e8835e39a07466183bb2ee
blacider <niurenpeng@niurenpeng.com>
2018-07-02 21:02:55 +0800

    [update] 提取业务公共代码和fire.js到入口包

--- find-entry.js
+++ find-entry.js
@@ -84,6 +84,12 @@ module.exports = function (directory = 'src/home') {
         require('common/css/main.css');
         require('common/css/icon.css');
         require('common/css/grid.css');
+
+        let tmp = '';
+
+        require.include(\`home/mod-common/\${tmp}.js\`); //业务公共部分代码，抽出，放到外层打包
+
+        require.include('home/mod-launch/fire'); //防止各个入口重复打包
         var param = require("common/lang/Object").fromQueryString(location.search.substring(1));
 
         param.m = param.m || 'mod-index/index';
@@ -94,3 +100,5 @@ module.exports = function (directory = 'src/home') {
 
     return Array.from(set);
 };
+
+module.exports();
